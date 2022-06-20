30586e555e1caf4d9ac15ae1100a5d324d98abf4
JustSomeDude2001 <42960562+JustSomeDude2001@users.noreply.github.com>
2022-06-20 20:52:15 +0300

    Basic jumping

---
+++ Assets/Scripts/Player/Controller.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Controller : MonoBehaviour
+{
+    private Jump _jump;
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        
+    }
+}
