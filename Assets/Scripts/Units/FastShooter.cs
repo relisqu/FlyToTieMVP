3a951e929e428b21315c5a19ec05e9ad957aafe7
y.kim <kim.yaroslavik@gmail.com>
2022-06-29 21:05:15 +0300

    Added FastShooter

---
+++ Assets/Scripts/Units/FastShooter.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using MoreMountains.Tools;
+using UnityEngine;
+
+namespace Units
+{
+    public class FastShooter : Shooter
+    {
+        [SerializeField] private int ProjectileCount;
+        [SerializeField] private float Delay;
+        [SerializeField] private Vector3 ProjectilePosOffset;
+        
+        private Coroutine _shooterCoroutine;
+        private bool _isShooting;
+        
+        private IEnumerator ShootNProjectiles(int n)
+        {
+            _isShooting = true;
+            for (int i = 0; i < n; i++)
+            {
+                Instantiate(Projectile, transform.position + ProjectilePosOffset, Quaternion.identity);
+                Projectile.SpawnProjectile();
+                yield return new WaitForSeconds(Delay);
+            }
+            _isShooting = false;
+        }
+        
+        public override void Shoot()
+        {
+            if (!_isShooting) 
+                _shooterCoroutine = StartCoroutine(ShootNProjectiles(ProjectileCount));
+        }
+
+        public override void StopShooting()
+        {
+            if (_shooterCoroutine != null)
+            {
+                StopCoroutine(_shooterCoroutine);
+                _isShooting = false;
+            }
+        }
+    }
+}
